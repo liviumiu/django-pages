f12c9c76aa3ac3a89594a09a6330e28601aa8689
Liviu Miu <liviumiu@gmail.com>
2019-02-16 08:49:04 +0200

    new name for project

--- pages_project/urls.py
+++ pages_project/urls.py
@@ -1,4 +1,4 @@
-"""pages_object URL Configuration
+"""pages_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
